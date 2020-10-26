2b8fce933f3475b7a76fbc15e2188345797f069f
ncardozo <n.cardozo@uniandes.edu.co>
2020-10-26 11:03:45 -0500

    changing tests to use jester
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- tests/cql-activation.test.js
+++ tests/cql-activation.test.js
@@ -1,38 +1,45 @@
-const visitor = require("../cql-cst-visitor")
+const Trait = require('traits').Trait
+const cop = require('context-traits')
+const visitor = require("../cql-interpreter")
 
+Context1 = new cop.Context({ name: "Context1"})
 
-var ast
+var statement
 
 //Test single context activation
 //This activation is global for all object instances
 const contextActivation = "activate: Context1"
-ast = visitor.toAst(contextActivation)
-console.log("---SINGLE ACTIVATION" + JSON.stringify(ast, null, "\t"))
+statement = visitor.visit(contextActivation)
+test(`---SINGLE GLOBAL ACTIVATION: ${contextActivation} to activate Context1`, () => {
+    eval(statement)
+    expect(Context1.isActive()).toBe(true)
+})
 
-
+/*
 //Single activation with name
 const contextActivation2 = "activate: name = Context1"
-ast = visitor.toAst(contextActivation2)
+ast = visitor.visit(contextActivation2)
 console.log("---KEYWORD ACTIVATION" + JSON.stringify(ast, null, "\t"))
 
 //Test single context activation
 //This activation is global for all object instances
 const contextDeactivation = "deactivate: Context1"
-ast = visitor.toAst(contextDeactivation)
+ast = visitor.visit(contextDeactivation)
 console.log("---SINGLE DEACTIVATION" + JSON.stringify(ast, null, "\t"))
 
 //Activate all contexts satisfying a range
 const rangeActivation = "activate: date between(20170412, 20170425)"
-ast = visitor.toAst(rangeActivation)
+ast = visitor.visit(rangeActivation)
 console.log("---RANGE ACTIVATION" + JSON.stringify(ast, null, "\t"))
 
 //Activate a contexts for an object instance
 const instanceActivation = "activate: name = Landscape for: objInstance"
-ast = visitor.toAst(instanceActivation)
+ast = visitor.visit(instanceActivation)
 console.log("---INSTANCE ACTIVATION" + JSON.stringify(ast, null, "\t"))
 
 //Activate a contexts for an object instance
 //This activation is local to an object type
 const classActivation = "activate: name = Landscape for: Bus"
-ast = visitor.toAst(classActivation)
+ast = visitor.visit(classActivation)
 console.log("---CLASS ACTIVATION" + JSON.stringify(ast, null, "\t"))
+*/
