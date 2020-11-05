489768238a76a9713e30a61ef37f0942f0ceade6
ncardozo <n.cardozo@uniandes.edu.co>
2020-11-05 10:32:50 -0500

    adding range tests
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- tests/cql-multi-range-activation.test.js
+++ tests/cql-multi-range-activation.test.js
@@ -15,14 +15,14 @@ cop.Manager.contexts.push(Context4)
 cop.Manager.contexts.push(Context5)
 
 
-let activationStatement = "activate: interface between(2,4)"
+let activationStatement = "activate: interface (between(2,3),between(6,9))"
 let statement = CQL.interpret(cql, activationStatement)
-test(`---OR RANGES ACTIVATION: ${activationStatement} to activate Contexts 1, 3 (of 5)`, () => {
+test(`---OR RANGES ACTIVATION: ${activationStatement} to activate Contexts 1, 2 (of 5)`, () => {
     //function to activate
     eval(statement)
     expect(Context1.isActive()).toBe(true)
     expect(Context2.isActive()).toBe(false)
-    expect(Context3.isActive()).toBe(true)
+    expect(Context3.isActive()).toBe(false)
     expect(Context4.isActive()).toBe(false)
-    expect(Context5.isActive()).toBe(false)
+    expect(Context5.isActive()).toBe(true)
 })
